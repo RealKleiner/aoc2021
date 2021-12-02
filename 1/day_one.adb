e27b9186b70e5250e2b90a4204086f4f94c7324b
Dag Jönsson <dag@jnsson.eu>
2021-12-02 13:56:55 +0100

    I was bored

--- 1/day_one.adb
+++ 1/day_one.adb
@@ -1,80 +1,65 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
-with Ada.Containers; use Ada.Containers;
-with Ada.Containers.Vectors;
+with Ada.Command_Line; use Ada.Command_Line;
 
 procedure Day_One is
-	File_Name : constant String := "input";
+	type T_Measurements is
+		array (1 .. 4) of Integer;
 
-	procedure Second is
-		package Integer_Vector is new 
-			Ada.Containers.Vectors(Index_Type => Natural, Element_Type => Integer);
-
-		function Compare_Windows(V : Integer_Vector.Vector; 
-								Index : Natural) return Boolean is
-		begin
-			return V(V.First_Index+(Index - 3)) < V(V.First_Index+Index);
-		end Compare_Windows;
-
-		File : File_Type;
-		Values : Integer_Vector.Vector;
-		Number_Increases : Integer;
-		Start : Natural := 3;
-
+	procedure First(Input : in Integer; Increases, Last : in out Integer) is
 	begin
-		Open(File, In_File, File_Name);
+		if Input > Last then
+			Increases := Increases + 1;
+		end if;
+		Last := Input;
+	end First;
+	
+	procedure Second(Input : in Integer; Increases, Count : in out Integer;
+					 Measurements : in out T_Measurements) is
+	begin
 
-		while not End_Of_File(File) loop 
-			Get(File, Number_Increases);
-			Skip_Line(File);
-			Values.Append(Number_Increases);
-		end loop;
-		Close(File);
-		Number_Increases := 0;
+		Measurements(Count) := Input;
+		Count := (Count mod T_Measurements'Last) + 1;
 
-		while Values.Last_Index >= Start loop
-			if Compare_Windows(Values, Start) then
-				Number_Increases := Number_Increases + 1;
-			end if;
-			Start := Start + 1;
-		end loop;
-		
-		Put_Line("=== Part 2 ===");
-		Put("There were ");
-		Put(Number_Increases, Width => 0);
-		Put(" increases in depth.");
-		
+		if Input > Measurements(Count) then
+			Increases := Increases + 1;
+		end if;
 	end Second;
 
-	procedure First is
-		File : File_Type;
-		Current_Measurment, Last_Measurment : Integer := 0;
-		Number_Increases : Integer := 0;
-	begin
-		Open(File, In_File, File_Name);
-		-- Read the first measurment to get started
-		Get(File, Last_Measurment);
 
-		while not End_Of_File(File) loop 
-			Get(File, Current_Measurment);
-			Skip_Line(File);
-			if Current_Measurment > Last_Measurment then
-				Number_Increases := Number_Increases + 1;
-			end if;
-			Last_Measurment := Current_Measurment;
-		end loop;
-		
-		Close(File);
-
-		Put_Line("=== Part 1 ===");
-		Put("There were ");
-		Put(Number_Increases, Width => 0);
-		Put(" increases in depth.");
-		
-	end First;
+	File : File_Type;
+	Current_Measurement : Integer;
+	Part1_Increases : Integer := 0;
+	Part1_Last_Measurement : Integer := Integer'Last;
+	Part2_Increases : Integer := 0;
+	Part2_Measurements : T_Measurements := (Integer'Last, Integer'Last, 
+											Integer'Last, Integer'Last);
+	Part2_Count : Integer := 1;
 begin
-	First;
-	New_Line(2);
-	Second;
+	if Argument_Count /= 1 then
+		Put_Line("Usage: " & Command_Name & " FILE_PATH");
+		return;
+	end if;
 
+	open(File, In_File, Argument(1));
+
+	while not End_Of_File(File) loop
+		Get(File, Current_Measurement);
+		Skip_Line(File);
+		First(Current_Measurement, Part1_Increases, Part1_Last_Measurement);
+		Second(Current_Measurement, Part2_Increases, Part2_Count, Part2_Measurements);
+	end loop;
+
+	close (File);
+
+	Put_Line("=== Part 1 ===");
+	Put("Number of increases: ");
+	Put(Part1_Increases, Width => 0);
+
+	New_Line(2);
+
+	Put_Line("=== Part 2 ===");
+	Put("Number of increases: ");
+	Put(Part2_Increases, Width => 0);
 end Day_One;
+
