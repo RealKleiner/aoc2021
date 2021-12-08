b836e72a9f02980d3e7b2a224a2362add84bd139
Dag Jönsson <dag@jnsson.eu>
2021-12-08 15:29:18 +0100

    a simple python skeleton

---
+++ skel.py
@@ -0,0 +1,33 @@
+import sys
+
+
+def read_data(file_path: str) -> list:
+    out = []
+    with open(file_path) as f:
+        out = f.read()
+
+    return out
+
+
+def part_one(data: list) -> int:
+    return 0
+
+
+def part_two(data: list) -> int:
+    return 0
+
+
+def main(file_path: str = "input"):
+    data = read_data(file_path)
+    print("=== Part 1 ===")
+    print(f"Result: {part_one(data)}")
+    print()
+    print("=== Part 2 ===")
+    print(f"Result: {part_two(data)}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        main(sys.argv[1])
+    else:
+        main()
